0f9fdcdc83a04ff4f58b9d8c046602f0565b7cc1
parvathi14 <e0376585@u.nus.edu>
2019-09-01 20:05:42 +0800

    Implemented Level-2: store and display text entered by user

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main (String[] args) {
@@ -7,13 +9,20 @@ public class Duke {
         System.out.println("Hello!, I'm Duke \n" +
                 "What can I do for you?");
 
+        List<String> list = new ArrayList<String>();
+
         while(input.hasNextLine()) {
             //System.out.println(input.nextLine());
             String s = new String(input.nextLine());
             if(s.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
+            } else if (s.equals("list")) {
+                for (int n = 0; n != list.size(); n += 1) {
+                    System.out.println((n + 1) + ". " + list.get(n));
+                }
             } else {
-                System.out.println(s);
+                System.out.println("added: " + s);
+                list.add(s);
             }
         }
     }
